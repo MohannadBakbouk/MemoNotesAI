a7e69d5f32e9d39a1ff3fbdfef15ec79fdc0b733
MohannadBakbouk <MohannadBakBouk@gmail.com>
2026-06-15 16:59:17 +0300

    Initial Commit

---
+++ MemoNotesAI/MemoNotesAIApp.swift
@@ -0,0 +1,17 @@
+//
+//  MemoNotesAIApp.swift
+//  MemoNotesAI
+//
+//  Created by Mohannad on 15/06/2026.
+//
+
+import SwiftUI
+
+@main
+struct MemoNotesAIApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
